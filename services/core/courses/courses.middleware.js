2538e20592bf3b97da165ee0d946bd1396173a5b
Diana Garbuzova <lessarea@gmail.com>
2020-05-15 13:38:40 +0300

    fix courses query

--- services/core/courses/courses.middleware.js
+++ services/core/courses/courses.middleware.js
@@ -7,10 +7,8 @@ module.exports = (server) => {
 	router.get('/courses', (req, res, next) => {
 		let url_parts = url.parse(req.originalUrl, true),
 			query = url_parts.query,
-			from = query.start || 0,
-			to = +query.start + +query.count,
-			sort = query.sort,
-			queryStr = query.query,
+			to = query.page * query.limit,
+			from = to - query.limit,
 			courses = server.db.getState().courses;
 		
 			if (!!query.textFragment) {
@@ -20,9 +18,11 @@ module.exports = (server) => {
 		if (courses.length < to || !to) {
 			to = courses.length;
 		}
-		courses = courses.slice(from, to);
-		
-		res.json(courses);
+
+		res.json({
+			data: courses.slice(from, to),
+			count: courses.length
+		});
 	});
 	
 	return router;
